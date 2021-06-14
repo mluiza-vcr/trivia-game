216c19843f229d793e61dc095e47d4e494e90422
Renzo Sevilha <sevilharenzo@gmail.com>
2021-06-14 15:56:40 -0300

    fix: provider store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,3 +1,10 @@
-const store = 'Oi';
+import { createStore } from 'redux';
+
+import rootReducer from '../reducer';
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 export default store;
